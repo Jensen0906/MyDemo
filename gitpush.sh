926fedf59eccec43c3ee644c83003f33063362eb
Jensen <2096832525@qq.com>
2023-10-09 16:47:01 +0800

    ---modify gitpush.sh to can commit space

--- gitpush.sh
+++ gitpush.sh
@@ -1,21 +1,3 @@
-function message() {
-    time=$(date "+%y%m%d.%H.%M")
-        echo "提交时间&输入的信息"
-    msg=$1
-    if [ ! $msg ]; then
-        echo "请输入commit信息"
-        exit 1
-    fi
-    git status .
-    git add .
-    git status .
-    git commit -m "${time}---${msg}"
-    echo "提交的信息为${time}---${msg}"
-    git push
-    echo "$msg"
-    exit 1
-}
-
 if [ $1 != -msg ]; then
     echo -e "请输入正确的命令\n"
     echo "不带参数       只提交时间"
