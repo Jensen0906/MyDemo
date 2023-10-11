cb690f45ab39d83c32e9f4ba4b2696000e5668d2
May <2096832525@qq.com>
2023-10-11 18:13:46 +0800

    231011.18.13 --- refine show pass icon

--- part_10/app/src/main/java/com/may/part_10/activities/LoginActivity.kt
+++ part_10/app/src/main/java/com/may/part_10/activities/LoginActivity.kt
@@ -2,9 +2,14 @@ package com.may.part_10.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.util.Log
+import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.ViewModelProvider
 import com.may.part_10.base.BaseActivity
 import com.may.part_10.constant.BaseWorkConst.REGISTER_SUCCESS_NAME
@@ -24,6 +29,7 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>() {
     private lateinit var userViewModel: UserViewModel
     private lateinit var registerLauncher: ActivityResultLauncher<Intent>
 
+    //  @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -32,6 +38,26 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>() {
         binding.user = user
 
         getRegisterLauncher()
+
+        binding.password.addTextChangedListener {
+            if (it?.toString().isNullOrEmpty()) {
+                Log.d(TAG, "onCreate: change no string")
+                binding.showPassword.visibility = View.INVISIBLE
+                binding.notShowPassword.visibility = View.INVISIBLE
+            } else if (binding.password.transformationMethod == HideReturnsTransformationMethod.getInstance()) {
+                Log.d(TAG, "onCreate: change has string need show password")
+                binding.showPassword.visibility = View.VISIBLE
+                binding.notShowPassword.visibility = View.INVISIBLE
+            } else if (binding.password.transformationMethod == PasswordTransformationMethod.getInstance()) {
+                Log.d(TAG, "onCreate: change has string need dismiss password")
+                binding.showPassword.visibility = View.INVISIBLE
+                binding.notShowPassword.visibility = View.VISIBLE
+            } else {
+                Log.d(TAG, "onCreate: just show imageview init")
+                binding.showPassword.visibility = View.INVISIBLE
+                binding.notShowPassword.visibility = View.VISIBLE
+            }
+        }
 
         binding.btnLogin.setOnClickListener {
             Log.d(TAG, user.toString())
@@ -47,6 +73,23 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>() {
         binding.tvRegister.setOnClickListener {
             registerLauncher.launch(Intent(this@LoginActivity, RegisterActivity::class.java))
         }
+
+        binding.showPassword.setOnClickListener {
+            Log.d(TAG, "onCreate: dismiss the password")
+            binding.password.run {
+
+                transformationMethod = PasswordTransformationMethod.getInstance()
+                if (text != null) setSelection(text!!.length)
+            }
+        }
+
+        binding.notShowPassword.setOnClickListener {
+            Log.d(TAG, "onCreate: show the password")
+            binding.password.run {
+                transformationMethod = HideReturnsTransformationMethod.getInstance()
+                if (text != null) setSelection(text!!.length)
+            }
+        }
     }
 
 
@@ -61,6 +104,7 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>() {
                             password = "" //clear password input before
                         }
                     }
+
                     else -> {}
                 }
             }
