c4675c394ba4bc504350250052779cb976e24d49
May <2096832525@qq.com>
2023-10-15 02:16:44 +0800

    231015.02.16 --- Hide url PATCH 3

---
+++ part_10/app/src/main/java/com/may/part_10/constant/Constants.kt
@@ -0,0 +1,18 @@
+package com.may.part_10.constant
+
+import android.text.method.PasswordTransformationMethod
+
+/**
+ * @Author Jensen
+ * @Date 2023/10/07
+ */
+object NetWorkConst {
+    const val BASE_URL = "*******"
+    const val SUCCESS_STATUS = 200
+}
+
+object BaseWorkConst {
+    const val TO_REGISTER_FOR_RESULT = 423
+    const val REGISTER_SUCCESS_NAME = "register_success_name"
+}
+
